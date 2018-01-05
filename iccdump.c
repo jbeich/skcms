6555ded7d4d9496c59cdaa1c5846685d513e4b8f
Brian Osman <brianosman@google.com>
2018-01-05 11:25:18 -0500

    Add iccdump target, placeholder tool for examining ICC files
    Bug:
    Change-Id: I53effd6a0af097627b30c9ff52a480e032326b34

---
+++ iccdump.c
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#include "skcms.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void error(const char* msg) {
+    printf("ERROR: %s\n", msg);
+    exit(1);
+}
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        printf("usage: %s <ICC filename>\n", argv[0]);
+        return 1;
+    }
+
+    FILE* fp = fopen(argv[1], "rb");
+    if (!fp) {
+        error("Unable to open input file");
+    }
+
+    fseek(fp, 0L, SEEK_END);
+    size_t len = ftell(fp);
+    rewind(fp);
+
+    void* buf = malloc(len);
+    size_t bytesRead = fread(buf, 1, len, fp);
+
+    skcms_ICCProfile profile;
+    if (!skcms_ICCProfile_parse(&profile, buf, bytesRead)) {
+        error("Unable to parse ICC profile");
+    }
+
+    return 0;
+}
