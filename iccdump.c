78b647557bd8b033d32b185a43b52c39557fb8be
Brian Osman <brianosman@google.com>
2018-01-05 14:42:21 -0500

    First pass of ICC (header) parsing code
    Includes functionality in iccdump that prints out most header fields,
    using the new accessor API. Example output:
    
    $ out/Debug/iccdump.exe AdobeRGB1998.icc
                    Size : 0x00000230 : 560
                 CMMType : 0x41444245 : 'ADBE'
                 Version : 0x02100000 : 2.1.0
            ProfileClass : 0x6D6E7472 : 'mntr'
          DataColorSpace : 0x52474220 : 'RGB '
                     PCS : 0x58595A20 : 'XYZ '
                Platform : 0x4150504C : 'APPL'
                   Flags : 0x00000000
      DeviceManufacturer : 0x6E6F6E65 : 'none'
             DeviceModel : 0x00000000 : ''
         RenderingIntent : 0x00000000 : 0
                 Creator : 0x41444245 : 'ADBE'
    
    Change-Id: I83f62c41dbcc13d7a4e398ac36f682bedede3905

--- iccdump.c
+++ iccdump.c
@@ -12,12 +12,30 @@
 #include "skcms.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 static void fatal(const char* msg) {
     fprintf(stderr, "ERROR: %s\n", msg);
     exit(1);
 }
 
+const char* skcms_signature_as_string(uint32_t sig) {
+    static char str[5];
+    memcpy(str, &sig, 4);
+    str[4] = 0;
+    return str;
+}
+/*
+bool skcms_ICCHeader_dump(skcms_ICCHeader* header) {
+    printf("size: %u\n", header->size);
+    printf("cmmType: %08X\n", header->cmmType);
+    printf("version: %u (%u.%u.%u)\n", header->version, header->version >> 24,
+        (header->version >> 20) & 0xf, (header->version >> 16) & 0xf);
+    printf("class: %08X '%s'\n", header->profileClass, skcms_signature_as_string(header->profileClass));
+    //    printf("data space: ")
+    return false;
+}
+*/
 int main(int argc, char** argv) {
     if (argc != 2) {
         printf("usage: %s <ICC filename>\n", argv[0]);
@@ -44,6 +62,36 @@ int main(int argc, char** argv) {
     if (!skcms_ICCProfile_parse(&profile, buf, bytesRead)) {
         fatal("Unable to parse ICC profile");
     }
+
+#define DUMP_SIG_FIELD(field) do {                                            \
+        uint32_t val = skcms_ICCProfile_get ## field(&profile);               \
+        printf("%20s : 0x%08X : '%c%c%c%c'\n", #field, val,                   \
+               val >> 24, (val >> 16) & 0xFF, (val >> 8) & 0xFF, val & 0xFF); \
+    } while(0)
+
+#define DUMP_INT_FIELD(field) do {                                            \
+        uint32_t val = skcms_ICCProfile_get ## field(&profile);               \
+        printf("%20s : 0x%08X : %u\n", #field, val, val);                     \
+    } while(0)
+
+#define DUMP_VER_FIELD(field) do {                                            \
+        uint32_t val = skcms_ICCProfile_get ## field(&profile);               \
+        printf("%20s : 0x%08X : %u.%u.%u\n", #field, val,                     \
+               val >> 24, (val >> 20) & 0xF, (val >> 16) & 0xF);              \
+    } while(0)
+
+    DUMP_INT_FIELD(Size);
+    DUMP_SIG_FIELD(CMMType);
+    DUMP_VER_FIELD(Version);
+    DUMP_SIG_FIELD(ProfileClass);
+    DUMP_SIG_FIELD(DataColorSpace);
+    DUMP_SIG_FIELD(PCS);
+    DUMP_SIG_FIELD(Platform);
+    printf("%20s : 0x%08X\n", "Flags", skcms_ICCProfile_getFlags(&profile));
+    DUMP_SIG_FIELD(DeviceManufacturer);
+    DUMP_SIG_FIELD(DeviceModel);
+    DUMP_INT_FIELD(RenderingIntent);
+    DUMP_SIG_FIELD(Creator);
 
     return 0;
 }
