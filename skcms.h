58cf83a29192a1b163234d0bcecafc2c4fad1f06
Brian Osman <brianosman@google.com>
2018-01-05 15:18:26 -0500

    First pass of ICC (header) parsing code
    Includes functionality in iccdump that prints out most header fields,
    using the new accessor API. Example output:
    
    $ out/Debug/iccdump.exe AdobeRGB1998.icc
                    Size : 0x00000230 : 560
                 CMMType : 0x41444245 : 'ADBE'
                 Version : 0x02100000 : 2.1.0
            ProfileClass : 0x6D6E7472 : 'mntr'
          DataColorSpace : 0x52474220 : 'RGB '
                     PCS : 0x58595A20 : 'XYZ '
                Platform : 0x4150504C : 'APPL'
                   Flags : 0x00000000
      DeviceManufacturer : 0x6E6F6E65 : 'none'
             DeviceModel : 0x00000000 : ''
         RenderingIntent : 0x00000000 : 0
                 Creator : 0x41444245 : 'ADBE'
    
    Change-Id: I83f62c41dbcc13d7a4e398ac36f682bedede3905

--- skcms.h
+++ skcms.h
@@ -11,6 +11,7 @@
 
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -36,12 +37,23 @@ typedef struct {
     float g, a,b,c,d,e,f;
 } skcms_TransferFunction;
 
+typedef struct {
+    uint16_t year;
+    uint16_t month;
+    uint16_t day;
+    uint16_t hour;
+    uint16_t minute;
+    uint16_t second;
+} skcms_ICCDateTime;
 
 typedef struct {
-    void* placeholder;
+    const uint8_t* buf;
+    size_t         len;  // Length of profile, may be smaller than len passed to parse
 } skcms_ICCProfile;
 
 // Parse an ICC profile and return true if possible, otherwise return false.
+// The buffer is not copied, it must remain valid as long as the skcms_ICCProfile
+// will be used.
 bool skcms_ICCProfile_parse(skcms_ICCProfile*, const void*, size_t);
 
 // If this profile's gamut can be represented by a 3x3 transform to XYZD50,
@@ -52,6 +64,22 @@ bool skcms_ICCProfile_toXYZD50(const skcms_ICCProfile*, skcms_Matrix3x3*);
 // identical and can be represented by a single skcms_TransferFunction, set it
 // (if non-NULL) and return true, otherwise return false.
 bool skcms_ICCProfile_getTransferFunction(const skcms_ICCProfile*, skcms_TransferFunction*);
+
+// Accessors for simple (non-prescribed) ICCProfile properties from the header
+uint32_t skcms_ICCProfile_getSize(const skcms_ICCProfile*);
+uint32_t skcms_ICCProfile_getCMMType(const skcms_ICCProfile*);
+uint32_t skcms_ICCProfile_getVersion(const skcms_ICCProfile*);
+uint32_t skcms_ICCProfile_getProfileClass(const skcms_ICCProfile*);
+uint32_t skcms_ICCProfile_getDataColorSpace(const skcms_ICCProfile*);
+uint32_t skcms_ICCProfile_getPCS(const skcms_ICCProfile*);
+void skcms_ICCProfile_getCreationDateTime(const skcms_ICCProfile*, skcms_ICCDateTime*);
+uint32_t skcms_ICCProfile_getPlatform(const skcms_ICCProfile*);
+uint32_t skcms_ICCProfile_getFlags(const skcms_ICCProfile*);
+uint32_t skcms_ICCProfile_getDeviceManufacturer(const skcms_ICCProfile*);
+uint32_t skcms_ICCProfile_getDeviceModel(const skcms_ICCProfile*);
+uint64_t skcms_ICCProfile_getDeviceAttributes(const skcms_ICCProfile*);
+uint32_t skcms_ICCProfile_getRenderingIntent(const skcms_ICCProfile*);
+uint32_t skcms_ICCProfile_getCreator(const skcms_ICCProfile*);
 
 // TODO: read table-based transfer functions
 
--- src/ICCProfile.c
+++ src/ICCProfile.c
@@ -5,13 +5,98 @@
  * found in the LICENSE file.
  */
 
+#include <assert.h>
+#include <math.h>
+
+#define skcms_make_tag(a, b, c, d)  (((a) << 24) | ((b) << 16) | ((c) << 8) | (d))
+
+static uint16_t read_big_u16(const uint8_t* ptr) {
+    return ptr[0] << 8 | ptr[1];
+}
+
+static uint32_t read_big_u32(const uint8_t* ptr) {
+    return ptr[0] << 24 | ptr[1] << 16 | ptr[2] << 8 | ptr[3];
+}
+
+static int32_t read_big_i32(const uint8_t* ptr) {
+    return (int32_t)read_big_u32(ptr);
+}
+
+static uint64_t read_big_u64(const uint8_t* ptr) {
+    uint64_t hi = read_big_u32(ptr);
+    uint64_t lo = read_big_u32(ptr + 4);
+    return hi << 32 | lo;
+}
+
+static float read_big_fixed(const uint8_t* ptr) {
+    return read_big_i32(ptr) / 65536.0f;
+}
+
+static bool almost_equal(float a, float b) {
+    return fabsf(a - b) < 0.01f;
+}
+
+typedef struct {
+    uint8_t Size[4];
+    uint8_t CMMType[4];
+    uint8_t Version[4];
+    uint8_t ProfileClass[4];
+    uint8_t DataColorSpace[4];
+    uint8_t PCS[4];
+    uint8_t CreationDateTime[12];
+    uint8_t Signature[4];
+    uint8_t Platform[4];
+    uint8_t Flags[4];
+    uint8_t DeviceManufacturer[4];
+    uint8_t DeviceModel[4];
+    uint8_t DeviceAttributes[8];
+    uint8_t RenderingIntent[4];
+    uint8_t IlluminantX[4];
+    uint8_t IlluminantY[4];
+    uint8_t IlluminantZ[4];
+    uint8_t Creator[4];
+    uint8_t ProfileId[16];
+    uint8_t Reserved[28];
+
+    // Tag count is not technically part of the header, but is required for a valid profile
+    uint8_t tagCount[4];
+} skcms_ICCHeader;
+
 bool skcms_ICCProfile_parse(skcms_ICCProfile* profile,
                             const void* buf,
                             size_t len) {
-    (void)profile;
-    (void)buf;
-    (void)len;
-    return false;
+    assert(sizeof(skcms_ICCHeader) == 132);
+
+    if (!profile) {
+        return false;
+    }
+    profile->buf = 0;
+    profile->len = 0;
+
+    if (len < sizeof(skcms_ICCHeader)) {
+        return false;
+    }
+
+    const skcms_ICCHeader* header = buf;
+    // Validate signature, size, and major version
+    if (read_big_u32(header->Signature) != skcms_make_tag('a', 'c', 's', 'p') ||
+        read_big_u32(header->Size) < len ||
+        (read_big_u32(header->Version) >> 24) > 4) {
+        return false;
+    }
+
+    if (!almost_equal(read_big_fixed(header->IlluminantX), 0.9642f) ||
+        !almost_equal(read_big_fixed(header->IlluminantY), 1.0f) ||
+        !almost_equal(read_big_fixed(header->IlluminantZ), 0.8249f)) {
+        return false;
+    }
+
+    // TODO: Validate tag count and table make sense (including last offset + size
+    // being smaller than header->size).
+
+    profile->buf = buf;
+    profile->len = read_big_u32(header->Size);
+    return true;
 }
 
 bool skcms_ICCProfile_toXYZD50(const skcms_ICCProfile* profile,
@@ -26,4 +111,42 @@ bool skcms_ICCProfile_getTransferFunction(const skcms_ICCProfile* profile,
     (void)profile;
     (void)transferFunction;
     return false;
+}
+
+#define MAKE_HEADER_GETTER_U32(field) \
+    uint32_t skcms_ICCProfile_get ## field (const skcms_ICCProfile* profile) { \
+        if (!profile || !profile->buf || !profile->len) { return 0; }         \
+        return read_big_u32(((const skcms_ICCHeader*)profile->buf)-> field);  \
+    }
+
+MAKE_HEADER_GETTER_U32(Size)
+MAKE_HEADER_GETTER_U32(CMMType)
+MAKE_HEADER_GETTER_U32(Version)
+MAKE_HEADER_GETTER_U32(ProfileClass)
+MAKE_HEADER_GETTER_U32(DataColorSpace)
+MAKE_HEADER_GETTER_U32(PCS)
+MAKE_HEADER_GETTER_U32(Platform)
+MAKE_HEADER_GETTER_U32(Flags)
+MAKE_HEADER_GETTER_U32(DeviceManufacturer)
+MAKE_HEADER_GETTER_U32(DeviceModel)
+MAKE_HEADER_GETTER_U32(RenderingIntent)
+MAKE_HEADER_GETTER_U32(Creator)
+
+#undef MAKE_HEADER_GETTER_U32
+
+uint64_t skcms_ICCProfile_getDeviceAttributes(const skcms_ICCProfile* profile) {
+    if (!profile || !profile->buf || !profile->len) { return 0; }
+    return read_big_u64(((const skcms_ICCHeader*)profile->buf)->DeviceAttributes);
+}
+
+void skcms_ICCProfile_getCreationDateTime(const skcms_ICCProfile* profile,
+                                          skcms_ICCDateTime* dateTime) {
+    if (!profile || !profile->buf || !profile->len || !dateTime) { return; }
+    const skcms_ICCHeader* header = (const skcms_ICCHeader*)profile->buf;
+    dateTime->year   = read_big_u16(header->CreationDateTime + 0);
+    dateTime->month  = read_big_u16(header->CreationDateTime + 2);
+    dateTime->day    = read_big_u16(header->CreationDateTime + 4);
+    dateTime->hour   = read_big_u16(header->CreationDateTime + 6);
+    dateTime->minute = read_big_u16(header->CreationDateTime + 8);
+    dateTime->second = read_big_u16(header->CreationDateTime + 10);
 }
