ee0b31fb93c15254920adc138a30d2f72c0e4911
Mike Klein <mtklein@chromium.org>
2018-01-16 17:41:35 -0500

    try doing work on Mac bots
    Change-Id: Ib5eb4378eefbc24e5e00cf36caceb8a9d587ab07

--- infra/bots/bot.py
+++ infra/bots/bot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python2.7
 
 import os
+import subprocess
 import sys
 
 print "Hello from {platform}!".format(platform=sys.platform)
@@ -10,3 +11,9 @@ for pkg in sys.argv[1:]:
   for root,dir,files in os.walk(pkg):
     for file in files:
       print os.path.join(root, file)
+
+if 'darwin' in sys.platform:
+  # Most Mac systems don't have GCC installed.  Disable those builds.
+  subprocess.check_call(['echo', 'disabled = true', '>>build/gcc'])
+  # Ninja will run the remaining Clang builds.
+  subprocess.check_call(['ninja', '-v'])
