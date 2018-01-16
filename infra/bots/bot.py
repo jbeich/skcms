d3828189784e406bcd6ea5f9344c153f9ba08b9b
Mike Klein <mtklein@chromium.org>
2018-01-16 17:41:35 -0500

    try doing work on Mac bots
    It's a simple as 1-2-3!
      1) disable GCC
      2) *cough*downloadandbuildNinja*cough*
      3) run Ninja
    See so easy!
    
    Change-Id: Ib5eb4378eefbc24e5e00cf36caceb8a9d587ab07

--- infra/bots/bot.py
+++ infra/bots/bot.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python2.7
 
 import os
+import subprocess
 import sys
+
+def call(cmd):
+  subprocess.check_call(cmd, shell=True)
 
 print "Hello from {platform}!".format(platform=sys.platform)
 
@@ -10,3 +14,18 @@ for pkg in sys.argv[1:]:
   for root,dir,files in os.walk(pkg):
     for file in files:
       print os.path.join(root, file)
+
+# TODO: temporary until I can figure out where our Ninja is...
+if not os.path.isdir('ninja'):
+  call('git clone git://github.com/ninja-build/ninja')
+call('cd ninja; git fetch')
+call('cd ninja; git checkout v1.8.2')
+call('cd ninja; python configure.py --bootstrap')
+
+if 'darwin' in sys.platform:
+  # Most Mac systems don't have GCC installed.  Disable those builds.
+  with open('build/gcc', 'a') as f:
+    print >>f, 'disabled = true'
+
+  # Ninja will run the remaining (Clang) builds.
+  call('ninja/ninja')
