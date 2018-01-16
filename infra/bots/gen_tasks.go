f88675300725b2d2568597e22e8d1bfba298aecc
Mike Klein <mtklein@chromium.org>
2018-01-16 16:35:26 -0500

    more bot work
      - streamline Makefile,infra_tests.py,gen_tasks.go down to
        just Makefile and gen_tasks.go.
      - add Mac bot
      - add an "skcms" bot that just depends on all the others
      - run bot.py
      - pass a package to bot.py on Linux
    
    Change-Id: I70b53c123134080da27024a6c80369e9d30abbaa
    Reviewed-on: https://skia-review.googlesource.com/95021
    Reviewed-by: Mike Klein <mtklein@chromium.org>

--- infra/bots/gen_tasks.go
+++ infra/bots/gen_tasks.go
@@ -4,75 +4,62 @@
 
 package main
 
-/*
-	Generate the tasks.json file.
-*/
-
 import (
-	"strings"
-
 	"go.skia.org/infra/task_scheduler/go/specs"
 )
 
 var (
-	// "Constants"
-
-	// Top-level list of all Jobs to run at each commit.
-	JOBS = []string{
-		"Dummy-Tests",
+	TASKS = []string{
+		"skcms-Linux",
+		"skcms-Mac",
 	}
 )
 
-// Dimensions for Linux GCE instances.
-func linuxGceDimensions() []string {
-	return []string{
-		"pool:Skia",
-		"os:Debian-9.2",
-		"gpu:none",
-		"cpu:x86-64-Haswell_GCE",
+func addTask(b *specs.TasksCfgBuilder, task string) {
+	dimensions := map[string][]string{
+		"skcms-Linux": []string{"cpu:x86-64-Haswell_GCE", "os:Debian-9.2"},
+		"skcms-Mac":   []string{"cpu:x86-64-E5-2697_v2", "os:Mac-10.13.2"},
 	}
-}
-
-// dummy creates a simple dummy task. Returns the name of the last Task in the
-// generated chain of Tasks, which the Job should add as a dependency.
-func dummy(b *specs.TasksCfgBuilder, name string) string {
-	task := &specs.TaskSpec{
-		Command: []string{
-			"/bin/echo", "hello world",
+	packages := map[string][]*specs.CipdPackage{
+		"skcms-Linux": []*specs.CipdPackage{
+			&specs.CipdPackage{
+				Name:    "skia/bots/clang_linux",
+				Path:    "clang_linux",
+				Version: "version:10",
+			},
 		},
-		Dimensions:  linuxGceDimensions(),
-		Isolate:     "dummy.isolate",
-		Priority:    0.8,
-		MaxAttempts: 1,
-	}
-	b.MustAddTask(name, task)
-	return name
-}
-
-// process generates Tasks and Jobs for the given Job name.
-func process(b *specs.TasksCfgBuilder, name string) {
-	deps := []string{}
-
-	// Dummy task.
-	if strings.Contains(name, "Dummy") {
-		deps = append(deps, dummy(b, name))
+		"skcms-Mac": []*specs.CipdPackage{},
 	}
 
-	// Add the Job spec.
-	b.MustAddJob(name, &specs.JobSpec{
+	command := []string{"python", "skcms/infra/bots/bot.py"}
+	for _, p := range packages[task] {
+		command = append(command, p.Path)
+	}
+
+	b.MustAddTask(task, &specs.TaskSpec{
+		CipdPackages: packages[task],
+		Command:      command,
+		Dimensions:   append(dimensions[task], "gpu:none", "pool:Skia"),
+		Isolate:      "bot.isolate",
+		Priority:     0.8,
+		MaxAttempts:  1,
+	})
+
+	b.MustAddJob(task, &specs.JobSpec{
 		Priority:  0.8,
-		TaskSpecs: deps,
+		TaskSpecs: []string{task},
 	})
 }
 
-// Regenerate the tasks.json file.
 func main() {
 	b := specs.MustNewTasksCfgBuilder()
-
-	// Create Tasks and Jobs.
-	for _, name := range JOBS {
-		process(b, name)
+	for _, task := range TASKS {
+		addTask(b, task)
 	}
 
+	b.MustAddJob("skcms", &specs.JobSpec{
+		Priority:  0.8,
+		TaskSpecs: TASKS,
+	})
 	b.MustFinish()
 }
