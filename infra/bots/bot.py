f3847c40c452283180a13af5eba6350794f35bf2
Mike Klein <mtklein@chromium.org>
2018-01-17 07:57:22 -0500

    use infra/ninja packages
    Change-Id: Ic25b412074a206ff7bb017f916e90927e0e13ffc

--- infra/bots/bot.py
+++ infra/bots/bot.py
@@ -11,13 +11,6 @@ def append(path, line):
   with open(path, 'a') as f:
     print >>f, line
 
-# TODO: temporary until I can figure out where our Ninja is...
-if not os.path.isdir('ninja'):
-  call('git clone git://github.com/ninja-build/ninja')
-call('cd ninja; git fetch')
-call('cd ninja; git checkout v1.8.2')
-call('cd ninja; python configure.py --bootstrap')
-
 print "Hello from {platform} in {cwd}!".format(platform=sys.platform,
                                                cwd=os.getcwd())
 
@@ -34,11 +27,11 @@ if 'linux' in sys.platform:
   append('skcms/build/gcc', 'cc = gcc')
 
   # Point to clang in our clang_linux package, also skipping ccache.
-  clang_linux = os.path.realpath(sys.argv[1])
+  clang_linux = os.path.realpath(sys.argv[2])
   append('skcms/build/clang', 'cc = {}/bin/clang'.format(clang_linux))
 
   # We're seeing an unexpected error when running gcc.tsan/tests:
   # FATAL: ThreadSanitizer: unexpected memory mapping 0x55e16e36c000-0x55e16e374000
   append('skcms/build/gcc.tsan', 'disabled = true')
 
-call('ninja/ninja -C skcms -k 0')
+call('{ninja}/ninja -C skcms -k 0'.format(ninja=sys.argv[1]))
