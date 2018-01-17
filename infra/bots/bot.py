d0d971c9c6270cccb5a6f85a9f90214d4734d40a
Mike Klein <mtklein@chromium.org>
2018-01-16 19:17:57 -0500

    make skcms-Linux bot real too
    Change-Id: Iec76004bb318ce8cce638c40bcd2ac562b75c10f

--- infra/bots/bot.py
+++ infra/bots/bot.py
@@ -7,12 +7,6 @@ import sys
 def call(cmd):
   subprocess.check_call(cmd, shell=True)
 
-for pkg in sys.argv[1:]:
-  print 'Contents of', pkg
-  for root,dir,files in os.walk(pkg):
-    for file in files:
-      print os.path.join(root, file)
-
 # TODO: temporary until I can figure out where our Ninja is...
 if not os.path.isdir('ninja'):
   call('git clone git://github.com/ninja-build/ninja')
@@ -24,7 +18,9 @@ print "Hello from {platform} in {cwd}!".format(platform=sys.platform,
                                                cwd=os.getcwd())
 
 if 'darwin' in sys.platform:
-  # Most Mac systems don't have GCC installed.  Disable those builds.
+  # Our Mac bots don't have ccache or GCC installed.
+  with open('skcms/build/clang', 'a') as f:
+    print >>f, 'cc = clang'
   with open('skcms/build/gcc', 'a') as f:
     print >>f, 'disabled = true'
 
@@ -32,8 +28,10 @@ if 'darwin' in sys.platform:
   with open('skcms/build/clang.lsan', 'a') as f:
     print >>f, 'disabled = true'
 
-  # Our Mac bots don't have ccache installed.
-  call("sed -i '' 's/ccache //g' skcms/build/*")
+if 'linux' in sys.platform:
+  # Point to clang in our clang_linux package.
+  clang_linux = os.path.realpath(argv[1])
+  with open('skcms/build/clang', 'a') as f:
+    print >>f, 'cc = {clang_linux}/bin/clang'.format(clang_linux=clang_linux)
 
-  # Ninja will run the remaining Clang builds.
-  call('ninja/ninja -C skcms -k 0')
+call('ninja/ninja -C skcms -k 0')
