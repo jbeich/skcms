731cd3108c95169e9450fee054bd85f8bdb252f3
Kevin Lubick <kjlubick@google.com>
2018-02-07 09:29:17 -0500

    Add two fuzz targets
    Bug: skia:
    Change-Id: Ide7fa4013efd5b5a4f4092eedb772e11e6586203

--- afl-fuzz.sh
+++ afl-fuzz.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-afl-clang skcms.c iccdump.c -o iccdump
+afl-clang skcms.c fuzz/fuzz_iccprofile_info.c -o fuzz_iccprofile_info -DIS_FUZZING_WITH_AFL -lm
 mkdir -p inputs
 find profiles -name '*.icc' -exec cp {} inputs \;
-exec afl-fuzz -i inputs -o findings ./iccdump @@
+exec afl-fuzz -i inputs -o findings ./fuzz_iccprofile_info @@
