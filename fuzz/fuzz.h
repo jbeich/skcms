f966891784ff1cb782a6b0f10762619a6bbe74ea
Kevin Lubick <kjlubick@google.com>
2018-02-07 09:29:17 -0500

    Add two fuzz targets
    These fuzz targets parse an ICC profile and get various
    information about it.
    
    The next fuzz target will tackle transforming to/from
    srgb.
    
    Bug: skia:7493
    Change-Id: Ide7fa4013efd5b5a4f4092eedb772e11e6586203

---
+++ fuzz/fuzz.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#pragma once
+
+// This allows one defined fuzz target to work for both afl and libfuzzer
+#ifdef IS_FUZZING_WITH_AFL
+    #include <stdio.h>
+    #include <stdlib.h>
+
+    #define DEF_FUZZ_MAIN(data, size)                                  \
+        int main(int argc, char** argv) {                              \
+            if (argc != 2) {                                           \
+                printf("usage: %s <ICC filename>\n", argv[0]);         \
+                return 1;                                              \
+            }                                                          \
+            FILE* fp = fopen(argv[1], "rb");                           \
+            if (!fp) {                                                 \
+                printf("Unable to open input file");                   \
+                return 1;                                              \
+            }                                                          \
+            fseek(fp, 0L, SEEK_END);                                   \
+            long slen = ftell(fp);                                     \
+            if (slen <= 0) {                                           \
+                printf("ftell failed");                                \
+                return 1;                                              \
+            }                                                          \
+            size_t len = (size_t)slen;                                 \
+            rewind(fp);                                                \
+            void* data = malloc(len);                                  \
+            size_t size = fread(data, 1, len, fp);                     \
+            fclose(fp);                                                \
+            if (size != len) {                                         \
+                printf("Unable to read file");                         \
+                return 1;                                              \
+            }
+#else
+    #define DEF_FUZZ_MAIN(data, size)                                  \
+        int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+#endif
---
+++ fuzz/fuzz_iccprofile_atf.c
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+// This fuzz target parses an ICCProfile and then computes the
+// approximateTransferFunction.  This is separate from fuzz_iccprofile_info
+// because it is a much more time-consuming function call.
+
+#include "../skcms.h"
+#include "fuzz.h"
+
+DEF_FUZZ_MAIN(data, size)
+    skcms_ICCProfile p;
+    if (!skcms_ICCProfile_parse(&p, data, size)) {
+        return 0;
+    }
+    skcms_TransferFunction tf;
+    float f = 0.05;
+    skcms_ICCProfile_approximateTransferFunction(&p, &tf, &f);
+    return 0;
+}
