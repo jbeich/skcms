e09ebdac79ba824647942f827c72335b5af59f49
Mike Klein <mtklein@chromium.org>
2018-02-07 11:48:30 -0500

    refactor fuzz/ a bit
    Change-Id: Idb1a209b1a1e264a9133b3c18243f4b289e0a04c

--- afl-fuzz.sh
+++ afl-fuzz.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-afl-clang skcms.c fuzz/fuzz_iccprofile_info.c -o fuzz_iccprofile_info -lm
+afl-clang skcms.c fuzz/fuzz_iccprofile_info.c fuzz/fuzz_main.c -lm -o fuzz_iccprofile_info
 mkdir -p inputs
 find profiles -name '*.icc' -exec cp {} inputs \;
 exec afl-fuzz -i inputs -o findings ./fuzz_iccprofile_info @@
