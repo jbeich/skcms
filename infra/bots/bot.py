bcdc87b313d007ff4e88d8363e8b16d88ca4f24f
Brian Osman <brianosman@google.com>
2018-02-13 14:34:21 -0500

    add skcms-Win bot
    Change-Id: Iadbc1bd0787869ea755562008798ee36bc123ac6

--- infra/bots/bot.py
+++ infra/bots/bot.py
@@ -4,6 +4,8 @@ import os
 import subprocess
 import sys
 
+ninja = sys.argv[1]
+
 def call(cmd):
   subprocess.check_call(cmd, shell=True)
 
@@ -21,9 +23,22 @@ if 'darwin' in sys.platform:
   # Our Mac bot toolchains are too old for LSAN.
   append('skcms/build/clang.lsan', 'disabled = true')
 
-if 'linux' in sys.platform:
+  call('{ninja}/ninja -C skcms -k 0'.format(ninja=ninja))
+
+elif 'linux' in sys.platform:
   # Point to clang in our clang_linux package.
   clang_linux = os.path.realpath(sys.argv[3])
   append('skcms/build/clang', 'cc = {}/bin/clang'.format(clang_linux))
 
-call('{ninja}/ninja -C skcms -k 0'.format(ninja=sys.argv[1]))
+  call('{ninja}/ninja -C skcms -k 0'.format(ninja=ninja))
+
+else:  # Windows
+  win_toolchain = os.path.realpath(sys.argv[2]) + '\\depot_tools\\win_toolchain\\vs_files\\a9e1098bba66d2acccc377d5ee81265910f29272'
+  os.environ['PATH'] = win_toolchain + '\\VC\\Tools\\MSVC\\14.11.25503\\bin\\HostX64\\x64;' + os.environ['PATH']
+  os.environ['INCLUDE'] = win_toolchain + '\\VC\\Tools\\MSVC\\14.11.25503\\include;'
+  os.environ['INCLUDE'] += win_toolchain + '\\win_sdk\\Include\\10.0.15063.0\\ucrt;'
+  os.environ['LIB'] = win_toolchain + '\\VC\\Tools\\MSVC\\14.11.25503\\lib\\x64;'
+  os.environ['LIB'] += win_toolchain + '\\win_sdk\\Lib\\10.0.15063.0\\um\\x64;'
+  os.environ['LIB'] += win_toolchain + '\\win_sdk\\Lib\\10.0.15063.0\\ucrt\\x64;'
+
+  call('{ninja}\\ninja.exe -C skcms -f build\\msvs -k 0'.format(ninja=ninja))
