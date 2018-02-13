17a78f9847098331cd8779cf4f270f9f50b55457
Mike Klein <mtklein@chromium.org>
2018-02-13 12:55:03 -0500

    add skcms-Win bot
    Change-Id: Iadbc1bd0787869ea755562008798ee36bc123ac6

--- infra/bots/bot.py
+++ infra/bots/bot.py
@@ -4,6 +4,8 @@ import os
 import subprocess
 import sys
 
+ninja = sys.argv[1]
+
 def call(cmd):
   subprocess.check_call(cmd, shell=True)
 
@@ -21,9 +23,17 @@ if 'darwin' in sys.platform:
   # Our Mac bot toolchains are too old for LSAN.
   append('skcms/build/clang.lsan', 'disabled = true')
 
-if 'linux' in sys.platform:
+  call('{ninja}/ninja -C skcms -k 0'.format(ninja=ninja))
+
+elif 'linux' in sys.platform:
   # Point to clang in our clang_linux package.
   clang_linux = os.path.realpath(sys.argv[3])
   append('skcms/build/clang', 'cc = {}/bin/clang'.format(clang_linux))
 
-call('{ninja}/ninja -C skcms -k 0'.format(ninja=sys.argv[1]))
+  call('{ninja}/ninja -C skcms -k 0'.format(ninja=ninja))
+
+else:  # Windows
+  win_toolchain = sys.argv[2]
+  # TODO: point the environment into win_toolchain as needed.
+
+  call('{ninja}/ninja.exe -C skcms -f build/msvs -k 0'.format(ninja=ninja))
