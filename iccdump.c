e43181d3c0959896eff7ff9641dd766e01cb32a1
Brian Osman <brianosman@google.com>
2018-02-27 14:18:27 -0500

    Split iccdump code into TestOnly.c helper
    Refactor profile parse testing to just compare iccdump output
    against golden txt files checked in alongside the icc files.
    
    Bug: skia:
    Change-Id: I432364468a6cee5cfe141d91267488ff6d9f05cb

--- iccdump.c
+++ iccdump.c
@@ -15,6 +15,7 @@
 
 #include "skcms.h"
 #include "src/Macros.h"
+#include "src/TestOnly.h"
 #include "src/TransferFunction.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -26,30 +27,6 @@ static void fatal(const char* msg) {
     exit(1);
 }
 
-static void signature_to_string(uint32_t sig, char* str) {
-    str[0] = (char)(sig >> 24);
-    str[1] = (char)(sig >> 16);
-    str[2] = (char)(sig >>  8);
-    str[3] = (char)(sig >>  0);
-    str[4] = 0;
-}
-
-static void dump_sig_field(const char* name, uint32_t val) {
-    char valStr[5];
-    signature_to_string(val, valStr);
-    printf("%20s : 0x%08X : '%s'\n", name, val, valStr);
-}
-
-static void dump_transfer_function(const char* name, const skcms_TransferFunction* tf) {
-    printf("%4s : %f, %f, %f, %f, %f, %f, %f", name,
-           (double)tf->g, (double)tf->a, (double)tf->b, (double)tf->c,
-           (double)tf->d, (double)tf->e, (double)tf->f);
-    if (skcms_IsSRGB(tf)) {
-        printf(" (sRGB)");
-    }
-    printf("\n");
-}
-
 static uint16_t read_big_u16(const uint8_t* ptr) {
     uint16_t be;
     memcpy(&be, ptr, sizeof(be));
@@ -58,34 +35,6 @@ static uint16_t read_big_u16(const uint8_t* ptr) {
 #else
     return __builtin_bswap16(be);
 #endif
-}
-
-static void dump_curve(const char* name, const skcms_Curve* curve, bool verbose) {
-    if (curve->table_entries) {
-        printf("%4s : %d-bit table with %u entries", name,
-               curve->table_8 ? 8 : 16, curve->table_entries);
-        if (verbose) {
-            char filename[32];
-            if (snprintf(filename, sizeof(filename), "%s.csv", name) < 0) {
-                return;
-            }
-            FILE* fp = fopen(filename, "wb");
-            if (fp) {
-                for (uint32_t i = 0; i < curve->table_entries; ++i) {
-                    double x = i / (curve->table_entries - 1.0);
-                    double t = curve->table_8
-                        ? curve->table_8[i] * (1.0 / 255)
-                        : read_big_u16(curve->table_16 + 2 * i) * (1.0 / 65535);
-                    fprintf(fp, "%f,%f\n", x, t);
-                }
-                fclose(fp);
-                printf(" (wrote to %s)", filename);
-            }
-        }
-        printf("\n");
-    } else {
-        dump_transfer_function(name, &curve->parametric);
-    }
 }
 
 static const double kSVGMarginLeft   = 100.0;
@@ -153,90 +102,20 @@ static void dump_curves_svg(const char* name, uint32_t num_curves, const skcms_C
     fclose(fp);
 }
 
-static void dump_curve_test_data(const skcms_Curve* c) {
-    const skcms_TransferFunction* p = &c->parametric;
-    printf("        { { %1.8ef, %1.8ef, %1.8ef, %1.8ef, %1.8ef, %1.8ef, %1.8ef },\n",
-           (double)p->g, (double)p->a, (double)p->b, (double)p->c,
-           (double)p->d, (double)p->e, (double)p->f);
-    printf("          %s, %s, %u },\n",
-           c->table_8 ? "NOT_NULL" : "NULL",
-           c->table_16 ? "NOT_NULL" : "NULL",
-           c->table_entries);
-}
-
-static void dump_curves_test_data(uint32_t num_channels, uint32_t num_curves,
-                                  const skcms_Curve* curves) {
-    printf("    %u, {\n", num_channels);
-    for (uint32_t i = 0; i < num_curves; ++i) {
-        dump_curve_test_data(curves + i);
-    }
-    printf("    },\n");
-}
-
-static void dump_a2b_test_data(const char* filename, const skcms_A2B* a2b) {
-    char* namecopy = malloc(strlen(filename) + 1);
-    if (!namecopy) {
-        fatal("malloc failed");
-    }
-    strcpy(namecopy, filename);
-    char* ext = strrchr(namecopy, '.');
-    if (ext) {
-        *ext = '\0';
-    }
-    char* basename = namecopy;
-    char* sep1 = strrchr(namecopy, '/');
-    if (sep1) {
-        basename = sep1 + 1;
-    }
-    char* sep2 = strrchr(namecopy, '\\');
-    if (sep2 && sep2 > basename) {
-        basename = sep2 + 1;
-    }
-
-    printf("static const skcms_A2B %s_A2B = {\n", basename);
-    dump_curves_test_data(a2b->input_channels, ARRAY_COUNT(a2b->input_curves), a2b->input_curves);
-    printf("    { %u, %u, %u, %u },\n",
-           a2b->grid_points[0], a2b->grid_points[1], a2b->grid_points[2], a2b->grid_points[3]);
-    printf("    %s, %s,\n", a2b->grid_8 ? "NOT_NULL" : "NULL", a2b->grid_16 ? "NOT_NULL" : "NULL");
-
-    dump_curves_test_data(a2b->matrix_channels, ARRAY_COUNT(a2b->matrix_curves),
-                          a2b->matrix_curves);
-    const skcms_Matrix3x4* m = &a2b->matrix;
-    printf("    { { { %1.8ef, %1.8ef, %1.8ef, %1.8ef },\n",
-        (double)m->vals[0][0], (double)m->vals[0][1], (double)m->vals[0][2], (double)m->vals[0][3]);
-    printf("        { %1.8ef, %1.8ef, %1.8ef, %1.8ef },\n",
-        (double)m->vals[1][0], (double)m->vals[1][1], (double)m->vals[1][2], (double)m->vals[1][3]);
-    printf("        { %1.8ef, %1.8ef, %1.8ef, %1.8ef } } },\n",
-        (double)m->vals[2][0], (double)m->vals[2][1], (double)m->vals[2][2], (double)m->vals[2][3]);
-
-    dump_curves_test_data(a2b->output_channels, ARRAY_COUNT(a2b->output_curves),
-                          a2b->output_curves);
-
-    printf("};\n");
-
-    free(namecopy);
-}
-
 int main(int argc, char** argv) {
     const char* filename = NULL;
-    bool verbose = false;
     bool svg = false;
-    bool test = false;
 
     for (int i = 1; i < argc; ++i) {
-        if (0 == strcmp(argv[i], "-v")) {
-            verbose = true;
-        } else if (0 == strcmp(argv[i], "-s")) {
+        if (0 == strcmp(argv[i], "-s")) {
             svg = true;
-        } else if (0 == strcmp(argv[i], "-t")) {
-            test = true;
         } else {
             filename = argv[i];
         }
     }
 
     if (!filename) {
-        printf("usage: %s [-t] [-v] [-s] <ICC filename>\n", argv[0]);
+        printf("usage: %s [-s] <ICC filename>\n", argv[0]);
         return 1;
     }
 
@@ -263,144 +142,27 @@ int main(int argc, char** argv) {
         fatal("Unable to read file");
     }
 
+    parse_and_dump_profile(buf, bytesRead, stdout);
+
     skcms_ICCProfile profile;
-    if (!skcms_Parse(buf, bytesRead, &profile)) {
-        fatal("Unable to parse ICC profile");
-    }
+    if (svg && skcms_Parse(buf, bytesRead, &profile)) {
+        if (profile.has_trc) {
+            dump_curves_svg(filename, 3, profile.trc);
+        }
 
-    if (test) {
         if (profile.has_A2B) {
-            dump_a2b_test_data(filename, &profile.A2B);
-        }
-        return 0;
-    }
-
-    printf("%20s : 0x%08X : %u\n", "Size", profile.size, profile.size);
-    dump_sig_field("CMM type", profile.cmm_type);
-    printf("%20s : 0x%08X : %u.%u.%u\n", "Version", profile.version,
-           profile.version >> 24, (profile.version >> 20) & 0xF, (profile.version >> 16) & 0xF);
-    dump_sig_field("Profile class", profile.profile_class);
-    dump_sig_field("Data color space", profile.data_color_space);
-    dump_sig_field("PCS", profile.pcs);
-    printf("%20s :            : %u-%02u-%02u %02u:%02u:%02u\n", "Creation date/time",
-           profile.creation_date_time.year, profile.creation_date_time.month,
-           profile.creation_date_time.day, profile.creation_date_time.hour,
-           profile.creation_date_time.minute, profile.creation_date_time.second);
-    dump_sig_field("Signature", profile.signature);
-    dump_sig_field("Platform", profile.platform);
-    printf("%20s : 0x%08X\n", "Flags", profile.flags);
-    dump_sig_field("Device manufacturer", profile.device_manufacturer);
-    dump_sig_field("Device model", profile.device_model);
-    printf("%20s : 0x%08X\n", "Device attributes", (uint32_t)profile.device_attributes);
-    printf("%20s : 0x%08X\n", "", (uint32_t)(profile.device_attributes >> 32));
-    printf("%20s : 0x%08X : %u\n", "Rendering intent", profile.rendering_intent,
-           profile.rendering_intent);
-    printf("%20s :            : %f\n", "Illuminant X", (double)profile.illuminant_X);
-    printf("%20s :            : %f\n", "Illuminant Y", (double)profile.illuminant_Y);
-    printf("%20s :            : %f\n", "Illuminant Z", (double)profile.illuminant_Z);
-    dump_sig_field("Creator", profile.creator);
-    printf("%20s : 0x%08X : %u\n", "Tag count", profile.tag_count, profile.tag_count);
-
-    printf("\n");
-
-    printf(" Tag    : Type   : Size   : Data\n");
-    printf(" ------ : ------ : ------ : --------\n");
-    for (uint32_t i = 0; i < profile.tag_count; ++i) {
-        skcms_ICCTag tag;
-        skcms_GetTagByIndex(&profile, i, &tag);
-        char tagSig[5];
-        char typeSig[5];
-        signature_to_string(tag.signature, tagSig);
-        signature_to_string(tag.type, typeSig);
-        printf(" '%s' : '%s' : %6u : %p\n", tagSig, typeSig, tag.size, (const void*)tag.buf);
-    }
-
-    printf("\n");
-
-    skcms_TransferFunction tf;
-    float max_error;
-    if (profile.has_tf) {
-        dump_transfer_function("TRC", &profile.tf);
-    } else if (skcms_ApproximateTransferFunction(&profile, &tf, &max_error)) {
-        printf("%4s : %f, %f, %f, %f, %f, %f, %f  (Max error: %f)\n", "~TRC",
-               (double)tf.g, (double)tf.a, (double)tf.b, (double)tf.c,
-               (double)tf.d, (double)tf.e, (double)tf.f, (double)max_error);
-    }
-
-    if (!profile.has_tf && profile.has_trc) {
-        const char* trcNames[3] = { "rTRC", "gTRC", "bTRC" };
-        for (int i = 0; i < 3; ++i) {
-            dump_curve(trcNames[i], &profile.trc[i], verbose);
-        }
-    }
-
-    if (svg && profile.has_trc) {
-        dump_curves_svg(filename, 3, profile.trc);
-    }
-
-    if (profile.has_toXYZD50) {
-        skcms_Matrix3x3 toXYZ = profile.toXYZD50;
-        printf(" XYZ : | %.7f %.7f %.7f |\n"
-               "       | %.7f %.7f %.7f |\n"
-               "       | %.7f %.7f %.7f |\n",
-               (double)toXYZ.vals[0][0], (double)toXYZ.vals[0][1], (double)toXYZ.vals[0][2],
-               (double)toXYZ.vals[1][0], (double)toXYZ.vals[1][1], (double)toXYZ.vals[1][2],
-               (double)toXYZ.vals[2][0], (double)toXYZ.vals[2][1], (double)toXYZ.vals[2][2]);
-    }
-
-    if (profile.has_A2B) {
-        const skcms_A2B* a2b = &profile.A2B;
-        printf(" A2B : %s%s\"B\"\n", a2b->input_channels ? "\"A\", CLUT, " : "",
-                                     a2b->matrix_channels ? "\"M\", Matrix, " : "");
-        if (a2b->input_channels) {
-            printf("%4s : %u inputs\n", "\"A\"", a2b->input_channels);
-            const char* curveNames[4] = { "A0", "A1", "A2", "A3" };
-            for (uint32_t i = 0; i < a2b->input_channels; ++i) {
-                dump_curve(curveNames[i], &a2b->input_curves[i], verbose);
-            }
-            printf("%4s : ", "CLUT");
-            const char* sep = "";
-            for (uint32_t i = 0; i < a2b->input_channels; ++i) {
-                printf("%s%u", sep, a2b->grid_points[i]);
-                sep = " x ";
-            }
-            printf(" (%d bpp)\n", a2b->grid_8 ? 8 : 16);
-
-            if (svg) {
+            const skcms_A2B* a2b = &profile.A2B;
+            if (a2b->input_channels) {
                 dump_curves_svg("A_curves", a2b->input_channels, a2b->input_curves);
             }
-        }
 
-        if (a2b->matrix_channels) {
-            printf("%4s : %u inputs\n", "\"M\"", a2b->matrix_channels);
-            const char* curveNames[4] = { "M0", "M1", "M2" };
-            for (uint32_t i = 0; i < a2b->matrix_channels; ++i) {
-                dump_curve(curveNames[i], &a2b->matrix_curves[i], verbose);
-            }
-            const skcms_Matrix3x4* m = &a2b->matrix;
-            printf("Mtrx : | %.7f %.7f %.7f %.7f |\n"
-                   "       | %.7f %.7f %.7f %.7f |\n"
-                   "       | %.7f %.7f %.7f %.7f |\n",
-                   (double)m->vals[0][0], (double)m->vals[0][1], (double)m->vals[0][2], (double)m->vals[0][3],
-                   (double)m->vals[1][0], (double)m->vals[1][1], (double)m->vals[1][2], (double)m->vals[1][3],
-                   (double)m->vals[2][0], (double)m->vals[2][1], (double)m->vals[2][2], (double)m->vals[2][3]);
-
-            if (svg) {
+            if (a2b->matrix_channels) {
                 dump_curves_svg("M_curves", a2b->matrix_channels, a2b->matrix_curves);
             }
-        }
 
-        {
-            printf("%4s : %u outputs\n", "\"B\"", a2b->output_channels);
-            const char* curveNames[3] = { "B0", "B1", "B2" };
-            for (uint32_t i = 0; i < a2b->output_channels; ++i) {
-                dump_curve(curveNames[i], &a2b->output_curves[i], verbose);
-            }
-
-            if (svg) {
-                dump_curves_svg("B_curves", a2b->output_channels, a2b->output_curves);
-            }
+            dump_curves_svg("B_curves", a2b->output_channels, a2b->output_curves);
         }
     }
+
     return 0;
 }
---
+++ src/TestOnly.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#pragma once
+
+#include <stdio.h>
+
+void parse_and_dump_profile(const void* buf, size_t size, FILE* fp);
