da8c4152866d44d17efe425a6143334ecca14f4d
Brian Osman <brianosman@google.com>
2018-03-07 13:08:42 -0500

    Remove two unnecessary files
    Bug: skia:
    Change-Id: I0881f58ceb19c0f1145ed7ca39d30786af0218d8
    Reviewed-on: https://skia-review.googlesource.com/112724
    Commit-Queue: Brian Osman <brianosman@google.com>
    Commit-Queue: Mike Klein <mtklein@google.com>
    Reviewed-by: Mike Klein <mtklein@google.com>

--- afl-fuzz.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-afl-clang skcms.c fuzz/fuzz_iccprofile_info.c fuzz/fuzz_main.c -lm -o fuzz_iccprofile_info
-mkdir -p inputs
-find profiles -name '*.icc' -exec cp {} inputs \;
-exec afl-fuzz -i inputs -o findings ./fuzz_iccprofile_info @@
