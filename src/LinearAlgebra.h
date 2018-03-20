495371e3531b945d2504d20fca98d6c119c9d9b8
Brian Osman <brianosman@google.com>
2018-03-20 14:31:42 -0400

    Large rewrite of approximate API
    New test, remove one old test, only supports approximating a single
    sampled function (but with no memory allocation).
    
    Bug: skia:
    Change-Id: Ia123558193dd6471838081c1928868756d1ded3a

--- src/LinearAlgebra.h
+++ src/LinearAlgebra.h
@@ -16,9 +16,13 @@ typedef struct { float vals[4][4]; } skcms_Matrix4x4;
 typedef struct { float vals[4]; } skcms_Vector4;
 typedef struct { float vals[3]; } skcms_Vector3;
 
+typedef struct { double vals[4][4]; } skcms_Matrix4x4d;
+typedef struct { double vals[4]; } skcms_Vector4d;
 
 // It is _not_ safe to alias the pointers to invert in-place.
 bool skcms_Matrix4x4_invert(const skcms_Matrix4x4*, skcms_Matrix4x4*);
 bool skcms_Matrix3x3_invert(const skcms_Matrix3x3*, skcms_Matrix3x3*);
+bool skcms_Matrix4x4d_invert(const skcms_Matrix4x4d*, skcms_Matrix4x4d*);
 
 skcms_Vector4 skcms_Matrix4x4_Vector4_mul(const skcms_Matrix4x4*, const skcms_Vector4*);
+skcms_Vector4d skcms_Matrix4x4d_Vector4d_mul(const skcms_Matrix4x4d*, const skcms_Vector4d*);
