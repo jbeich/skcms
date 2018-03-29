bd4c3b25abdf99478f5b42717877c5a355bc0d4c
Mike Klein <mtklein@chromium.org>
2018-03-29 11:02:42 -0400

    add smoke test for skcms_Transform() changes
    Change-Id: I109b9189920c28eb0a6b428b0dc6a333f2520a8c

--- test_only.c
+++ test_only.c
@@ -15,6 +15,83 @@
 #include <stdlib.h>
 #include <string.h>
 
+static void dump_to_XYZD50(FILE* fp, const skcms_ICCProfile* profile) {
+    // Here are 252 of a random shuffle of all possible bytes.
+    // 252 is evenly divisible by 3 and 4.  Only 192, 10, 241, and 43 are missing.
+    static const uint8_t k252_bytes[] = {
+        8, 179, 128, 204, 253, 38, 134, 184, 68, 102, 32, 138, 99, 39, 169, 215,
+        119, 26, 3, 223, 95, 239, 52, 132, 114, 74, 81, 234, 97, 116, 244, 205, 30,
+        154, 173, 12, 51, 159, 122, 153, 61, 226, 236, 178, 229, 55, 181, 220, 191,
+        194, 160, 126, 168, 82, 131, 18, 180, 245, 163, 22, 246, 69, 235, 252, 57,
+        108, 14, 6, 152, 240, 255, 171, 242, 20, 227, 177, 238, 96, 85, 16, 211,
+        70, 200, 149, 155, 146, 127, 145, 100, 151, 109, 19, 165, 208, 195, 164,
+        137, 254, 182, 248, 64, 201, 45, 209, 5, 147, 207, 210, 113, 162, 83, 225,
+        9, 31, 15, 231, 115, 37, 58, 53, 24, 49, 197, 56, 120, 172, 48, 21, 214,
+        129, 111, 11, 50, 187, 196, 34, 60, 103, 71, 144, 47, 203, 77, 80, 232,
+        140, 222, 250, 206, 166, 247, 139, 249, 221, 72, 106, 27, 199, 117, 54,
+        219, 135, 118, 40, 79, 41, 251, 46, 93, 212, 92, 233, 148, 28, 121, 63,
+        123, 158, 105, 59, 29, 42, 143, 23, 0, 107, 176, 87, 104, 183, 156, 193,
+        189, 90, 188, 65, 190, 17, 198, 7, 186, 161, 1, 124, 78, 125, 170, 133,
+        174, 218, 67, 157, 75, 101, 89, 217, 62, 33, 141, 228, 25, 35, 91, 230, 4,
+        2, 13, 73, 86, 167, 237, 84, 243, 44, 185, 66, 130, 110, 150, 142, 216, 88,
+        112, 36, 224, 136, 202, 76, 94, 98, 175, 213
+    };
+
+    static const skcms_ICCProfile XYZD50 = {
+        .buffer           = NULL,
+        .size             = 0,
+        .data_color_space = 0x52474220, // 'RGB '
+        .pcs              = 0x58595A20, // 'XYZ '
+        .tag_count        = 0,
+        .has_trc          = true,
+        .has_toXYZD50     = true,
+        .has_A2B          = false,
+
+        .trc = {
+            {{{0, {1,1,0,0,0,0,0}}}},
+            {{{0, {1,1,0,0,0,0,0}}}},
+            {{{0, {1,1,0,0,0,0,0}}}},
+        },
+
+        .toXYZD50 = {{
+            {1,0,0},
+            {0,1,0},
+            {0,0,1},
+        }},
+    };
+
+    // Interpret as RGB_888 if data color space is RGB or GRAY, RGBA_8888 if CMYK.
+    skcms_PixelFormat fmt = skcms_PixelFormat_RGB_888;
+    size_t npixels = 84;
+    if (profile->data_color_space == 0x434D594B/*CMYK*/) {
+        fmt = skcms_PixelFormat_RGBA_8888;
+        npixels = 63;
+    }
+
+    uint8_t dst[252];
+
+    if (!skcms_Transform(k252_bytes,                fmt, skcms_AlphaFormat_Unpremul, profile,
+                         dst, skcms_PixelFormat_RGB_888, skcms_AlphaFormat_Unpremul, &XYZD50,
+                         npixels)) {
+        fprintf(fp, "We can parse this profile, but not transform it XYZD50!\n");
+    }
+
+    fprintf(fp, "252 random bytes transformed to linear XYZD50 bytes:\n");
+    for (size_t i = 0; i < npixels; i += 7) {
+        fprintf(fp, "\t"
+                    "%02x%02x%02x %02x%02x%02x %02x%02x%02x %02x%02x%02x "
+                    "%02x%02x%02x %02x%02x%02x %02x%02x%02x\n",
+                dst[3*i+ 0], dst[3*i+ 1], dst[3*i+ 2],
+                dst[3*i+ 3], dst[3*i+ 4], dst[3*i+ 5],
+                dst[3*i+ 6], dst[3*i+ 7], dst[3*i+ 8],
+                dst[3*i+ 9], dst[3*i+10], dst[3*i+11],
+                dst[3*i+12], dst[3*i+13], dst[3*i+14],
+                dst[3*i+15], dst[3*i+16], dst[3*i+17],
+                dst[3*i+18], dst[3*i+19], dst[3*i+20]);
+    }
+}
+
+
 static void signature_to_string(uint32_t sig, char* str) {
     str[0] = (char)((sig >> 24) & 0xFF);
     str[1] = (char)((sig >> 16) & 0xFF);
@@ -196,6 +273,8 @@ void dump_profile(const skcms_ICCProfile* profile, FILE* fp, bool for_unit_test)
             }
         }
     }
+
+    dump_to_XYZD50(fp, profile);
 }
 
 bool load_file_fp(FILE* fp, void** buf, size_t* len) {
