36efd4bfced0be2dfb93ac322459e852671ddd0f
Brian Osman <brianosman@google.com>
2018-04-05 11:09:23 -0400

    Run regression with two linear tolerances
    Rationale: Tables that were generated from a parametric function
    should have their linear portion fit a line almost perfectly.
    Because of rounding when converting to 16 bit fixed point, they
    may 'wobble' by 1 bit in that representation. 1.5 / 64k captures
    that wobble, but otherwise requires that they remain linear.
    For example, quantizing sRGB to a 1024 entry table, this lets us
    match the linear portion exactly.
    
    For other tables (eg calibrated displays, etc...), there can be
    significant noise and variance in the "linear" portion of the
    display's response curve. If we're operating at 8-bit precision,
    then we want no more than (1/512 == 0.5/256) error, so this is
    a good upper bound on tolerance.
    
    Luckily, our most difficult test profile performs very well
    with this second tolerance (DisplayCal_ASUS_NonMonotonic).
    
    Change-Id: I5ad583fe68b315f85785f21dce949a2978b9f983

--- src/TransferFunction.c
+++ src/TransferFunction.c
@@ -242,14 +242,11 @@ static bool tf_solve_nonlinear(skcms_TableFunc* t, const void* ctx, int start, i
     return true;
 }
 
-bool skcms_TransferFunction_approximate(skcms_TableFunc* t, const void* ctx, int n,
-                                        skcms_TransferFunction* fn, float* max_error) {
-    if (n < 2) {
-        return false;
-    }
-
+// Returns the number of points that are approximated by the line, to within tol.
+static int tf_fit_linear(skcms_TableFunc* t, const void* ctx, int n, float tol,
+                         skcms_TransferFunction* fn) {
     // Idea: We fit the first N points to the linear portion of the TF. We want the line to pass
-    // through the first and last points exactly, and have a maximum error of kLinearTolerance.
+    // through the first and last points exactly.
     //
     // We walk along the points, and find the minimum and maximum slope of the line before the
     // error would exceed our tolerance. Once the range [slope_min, slope_max] would be empty,
@@ -258,10 +255,6 @@ bool skcms_TransferFunction_approximate(skcms_TableFunc* t, const void* ctx, int
     // However, some points error intervals' may intersect the running interval, but not lie within
     // it. So we keep track of the last point we saw that is a valid candidate for being the end
     // point, and once the search is done, back up to build the line through *that* point.
-
-    // Assume that the values started out as .16 fixed point, and we want them to be almost exactly
-    // linear in that representation.
-    const float kLinearTolerance = 1.5f / 65535.0f;
     const float x_scale = 1.0f / (n - 1);
 
     int lin_points = 1;
@@ -271,8 +264,8 @@ bool skcms_TransferFunction_approximate(skcms_TableFunc* t, const void* ctx, int
     for (int i = 1; i < n; ++i) {
         float xi = i * x_scale;
         float yi = t(i, ctx);
-        float slope_max_i = (yi + kLinearTolerance - fn->f) / xi;
-        float slope_min_i = (yi - kLinearTolerance - fn->f) / xi;
+        float slope_max_i = (yi + tol - fn->f) / xi;
+        float slope_min_i = (yi - tol - fn->f) / xi;
         if (slope_max_i < slope_min || slope_max < slope_min_i) {
             // Slope intervals no longer overlap.
             break;
@@ -288,48 +281,81 @@ bool skcms_TransferFunction_approximate(skcms_TableFunc* t, const void* ctx, int
 
     // Set D to the last point from above
     fn->d = (lin_points - 1) * x_scale;
+    return lin_points;
+}
 
-    // If the entire data set was linear, move the coefficients to the nonlinear portion with
-    // G == 1. This lets use a canonical representation with D == 0.
-    if (lin_points == n) {
-        fn->g = 1;
-        fn->b = fn->f;
-        fn->a = fn->c;
-        fn->c = fn->d = fn->e = fn->f = 0;
-    } else if (lin_points == n - 1) {
-        // Degenerate case with only two points in the nonlinear segment. Solve directly.
-        fn->g = 1;
-        fn->a = (t(n - 1, ctx) - t(n - 2, ctx)) * (n - 1);
-        fn->b = t(n - 2, ctx) - (fn->a * (n - 2) * x_scale);
-        fn->e = 0;
-    } else {
-        // Do a nonlinear regression on the nonlinear segment. Include the 'D' point in the
-        // nonlinear regression, so the two pieces are more likely to line up.
-        int start = lin_points > 0 ? lin_points - 1 : 0;
+static float tf_max_error(skcms_TableFunc* t, const void* ctx, int n,
+                          const skcms_TransferFunction* fn) {
+    const float x_scale = 1.0f / (n - 1);
+    float max_error = 0;
+    for (int i = 0; i < n; ++i) {
+        float xi = i * x_scale;
+        float fn_of_xi = skcms_TransferFunction_eval(fn, xi);
+        float error_at_xi = fabsf_(t(i, ctx) - fn_of_xi);
+        max_error = fmaxf_(max_error, error_at_xi);
+    }
+    return max_error;
+}
 
-        // We need G to be in right vicinity, or the regression may not converge. Solve exactly for
-        // for midpoint of the nonlinear range, assuming B = E = 0 & A = 1.
-        int mid = (start + n) / 2;
-        float mid_x = mid / (n - 1.0f);
-        float mid_y = t(mid, ctx);
-        fn->g = log2f_(mid_y) / log2f_(mid_x);;
-        fn->a = 1;
-        fn->b = 0;
-        fn->e = 0;
+bool skcms_TransferFunction_approximate(skcms_TableFunc* t, const void* ctx, int n,
+                                        skcms_TransferFunction* fn, float* max_error) {
+    if (n < 2) {
+        return false;
+    }
 
-        if (!tf_solve_nonlinear(t, ctx, start, n, fn)) {
-            return false;
+    const float x_scale = 1.0f / (n - 1);
+    const float kTolerances[] = { 1.5f / 65535.0f, 1.0f / 512.0f };
+    float min_error = INFINITY_;
+
+    for (int tol = 0; tol < ARRAY_COUNT(kTolerances); ++tol) {
+        skcms_TransferFunction tf;
+        int lin_points = tf_fit_linear(t, ctx, n,kTolerances[tol], &tf);
+
+        // If the entire data set was linear, move the coefficients to the nonlinear portion with
+        // G == 1. This lets use a canonical representation with D == 0.
+        if (lin_points == n) {
+            tf.g = 1;
+            tf.b = tf.f;
+            tf.a = tf.c;
+            tf.c = tf.d = tf.e = tf.f = 0;
+        } else if (lin_points == n - 1) {
+            // Degenerate case with only two points in the nonlinear segment. Solve directly.
+            tf.g = 1;
+            tf.a = (t(n - 1, ctx) - t(n - 2, ctx)) * (n - 1);
+            tf.b = t(n - 2, ctx) - (tf.a * (n - 2) * x_scale);
+            tf.e = 0;
+        } else {
+            // Do a nonlinear regression on the nonlinear segment. Include the 'D' point in the
+            // nonlinear regression, so the two pieces are more likely to line up.
+            int start = lin_points > 0 ? lin_points - 1 : 0;
+
+            // We need G to be in right vicinity, or the regression may not converge. Solve exactly for
+            // for midpoint of the nonlinear range, assuming B = E = 0 & A = 1.
+            int mid = (start + n) / 2;
+            float mid_x = mid / (n - 1.0f);
+            float mid_y = t(mid, ctx);
+            tf.g = log2f_(mid_y) / log2f_(mid_x);;
+            tf.a = 1;
+            tf.b = 0;
+            tf.e = 0;
+
+            if (!tf_solve_nonlinear(t, ctx, start, n, &tf)) {
+                continue;
+            }
+        }
+
+        float err = tf_max_error(t, ctx, n, &tf);
+        if (min_error > err) {
+            min_error = err;
+            *fn = tf;
         }
     }
 
+    if (!isfinitef_(best_error)) {
+        return false;
+    }
     if (max_error) {
-        *max_error = 0;
-        for (int i = 0; i < n; ++i) {
-            float xi = i * x_scale;
-            float fn_of_xi = skcms_TransferFunction_eval(fn, xi);
-            float error_at_xi = fabsf_(t(i, ctx) - fn_of_xi);
-            *max_error = fmaxf_(*max_error, error_at_xi);
-        }
+        *max_error = best_error;
     }
 
     return true;
