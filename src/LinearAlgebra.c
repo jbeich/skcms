4fcc05174dfb1852cf245861d731f2c191ec1af5
Brian Osman <brianosman@google.com>
2018-04-05 09:27:28 -0400

    Go back to float precision throughout approximation code
    Change-Id: Id7dc92fa6d1c916a1511931d3422622b7127c357

--- src/LinearAlgebra.c
+++ src/LinearAlgebra.c
@@ -12,7 +12,7 @@
 static bool Matrix4x4_isfinite(const skcms_Matrix4x4* m) {
     for (int r = 0; r < 4; ++r)
     for (int c = 0; c < 4; ++c) {
-        if (!isfinited_(m->vals[r][c])) {
+        if (!isfinitef_((float)m->vals[r][c])) {
             return false;
         }
     }
@@ -20,22 +20,22 @@ static bool Matrix4x4_isfinite(const skcms_Matrix4x4* m) {
 }
 
 bool skcms_Matrix4x4_invert(const skcms_Matrix4x4* src, skcms_Matrix4x4* dst) {
-    double a00 = src->vals[0][0],
-           a01 = src->vals[1][0],
-           a02 = src->vals[2][0],
-           a03 = src->vals[3][0],
-           a10 = src->vals[0][1],
-           a11 = src->vals[1][1],
-           a12 = src->vals[2][1],
-           a13 = src->vals[3][1],
-           a20 = src->vals[0][2],
-           a21 = src->vals[1][2],
-           a22 = src->vals[2][2],
-           a23 = src->vals[3][2],
-           a30 = src->vals[0][3],
-           a31 = src->vals[1][3],
-           a32 = src->vals[2][3],
-           a33 = src->vals[3][3];
+    double a00 = (double)src->vals[0][0],
+           a01 = (double)src->vals[1][0],
+           a02 = (double)src->vals[2][0],
+           a03 = (double)src->vals[3][0],
+           a10 = (double)src->vals[0][1],
+           a11 = (double)src->vals[1][1],
+           a12 = (double)src->vals[2][1],
+           a13 = (double)src->vals[3][1],
+           a20 = (double)src->vals[0][2],
+           a21 = (double)src->vals[1][2],
+           a22 = (double)src->vals[2][2],
+           a23 = (double)src->vals[3][2],
+           a30 = (double)src->vals[0][3],
+           a31 = (double)src->vals[1][3],
+           a32 = (double)src->vals[2][3],
+           a33 = (double)src->vals[3][3];
 
     double b00 = a00*a11 - a01*a10,
            b01 = a00*a12 - a02*a10,
@@ -62,7 +62,7 @@ bool skcms_Matrix4x4_invert(const skcms_Matrix4x4* src, skcms_Matrix4x4* dst) {
     }
 
     double invdet = 1.0 / determinant;
-    if (!isfinited_(invdet)) {
+    if (!isfinitef_((float)invdet)) {
         return false;
     }
 
@@ -79,22 +79,22 @@ bool skcms_Matrix4x4_invert(const skcms_Matrix4x4* src, skcms_Matrix4x4* dst) {
     b10 *= invdet;
     b11 *= invdet;
 
-    dst->vals[0][0] = ( a11*b11 - a12*b10 + a13*b09 );
-    dst->vals[1][0] = ( a02*b10 - a01*b11 - a03*b09 );
-    dst->vals[2][0] = ( a31*b05 - a32*b04 + a33*b03 );
-    dst->vals[3][0] = ( a22*b04 - a21*b05 - a23*b03 );
-    dst->vals[0][1] = ( a12*b08 - a10*b11 - a13*b07 );
-    dst->vals[1][1] = ( a00*b11 - a02*b08 + a03*b07 );
-    dst->vals[2][1] = ( a32*b02 - a30*b05 - a33*b01 );
-    dst->vals[3][1] = ( a20*b05 - a22*b02 + a23*b01 );
-    dst->vals[0][2] = ( a10*b10 - a11*b08 + a13*b06 );
-    dst->vals[1][2] = ( a01*b08 - a00*b10 - a03*b06 );
-    dst->vals[2][2] = ( a30*b04 - a31*b02 + a33*b00 );
-    dst->vals[3][2] = ( a21*b02 - a20*b04 - a23*b00 );
-    dst->vals[0][3] = ( a11*b07 - a10*b09 - a12*b06 );
-    dst->vals[1][3] = ( a00*b09 - a01*b07 + a02*b06 );
-    dst->vals[2][3] = ( a31*b01 - a30*b03 - a32*b00 );
-    dst->vals[3][3] = ( a20*b03 - a21*b01 + a22*b00 );
+    dst->vals[0][0] = (float)( a11*b11 - a12*b10 + a13*b09 );
+    dst->vals[1][0] = (float)( a02*b10 - a01*b11 - a03*b09 );
+    dst->vals[2][0] = (float)( a31*b05 - a32*b04 + a33*b03 );
+    dst->vals[3][0] = (float)( a22*b04 - a21*b05 - a23*b03 );
+    dst->vals[0][1] = (float)( a12*b08 - a10*b11 - a13*b07 );
+    dst->vals[1][1] = (float)( a00*b11 - a02*b08 + a03*b07 );
+    dst->vals[2][1] = (float)( a32*b02 - a30*b05 - a33*b01 );
+    dst->vals[3][1] = (float)( a20*b05 - a22*b02 + a23*b01 );
+    dst->vals[0][2] = (float)( a10*b10 - a11*b08 + a13*b06 );
+    dst->vals[1][2] = (float)( a01*b08 - a00*b10 - a03*b06 );
+    dst->vals[2][2] = (float)( a30*b04 - a31*b02 + a33*b00 );
+    dst->vals[3][2] = (float)( a21*b02 - a20*b04 - a23*b00 );
+    dst->vals[0][3] = (float)( a11*b07 - a10*b09 - a12*b06 );
+    dst->vals[1][3] = (float)( a00*b09 - a01*b07 + a02*b06 );
+    dst->vals[2][3] = (float)( a31*b01 - a30*b03 - a32*b00 );
+    dst->vals[3][3] = (float)( a20*b03 - a21*b01 + a22*b00 );
 
     return Matrix4x4_isfinite(dst);
 }
@@ -102,10 +102,10 @@ bool skcms_Matrix4x4_invert(const skcms_Matrix4x4* src, skcms_Matrix4x4* dst) {
 bool skcms_Matrix3x3_invert(const skcms_Matrix3x3* src, skcms_Matrix3x3* dst) {
     // TODO: I am being _very_ lazy.
     skcms_Matrix4x4 m = {{
-        { (double)src->vals[0][0], (double)src->vals[0][1], (double)src->vals[0][2], 0.0 },
-        { (double)src->vals[1][0], (double)src->vals[1][1], (double)src->vals[1][2], 0.0 },
-        { (double)src->vals[2][0], (double)src->vals[2][1], (double)src->vals[2][2], 0.0 },
-        {                     0.0,                     0.0,                     0.0, 1.0 },
+        { src->vals[0][0], src->vals[0][1], src->vals[0][2], 0.0f },
+        { src->vals[1][0], src->vals[1][1], src->vals[1][2], 0.0f },
+        { src->vals[2][0], src->vals[2][1], src->vals[2][2], 0.0f },
+        {            0.0f,            0.0f,            0.0f, 1.0f },
     }};
 
     skcms_Matrix4x4 inv;
@@ -114,9 +114,9 @@ bool skcms_Matrix3x3_invert(const skcms_Matrix3x3* src, skcms_Matrix3x3* dst) {
     }
 
     *dst = (skcms_Matrix3x3){{
-        { (float)inv.vals[0][0], (float)inv.vals[0][1], (float)inv.vals[0][2] },
-        { (float)inv.vals[1][0], (float)inv.vals[1][1], (float)inv.vals[1][2] },
-        { (float)inv.vals[2][0], (float)inv.vals[2][1], (float)inv.vals[2][2] },
+        { inv.vals[0][0], inv.vals[0][1], inv.vals[0][2] },
+        { inv.vals[1][0], inv.vals[1][1], inv.vals[1][2] },
+        { inv.vals[2][0], inv.vals[2][1], inv.vals[2][2] },
     }};
     return true;
 }
