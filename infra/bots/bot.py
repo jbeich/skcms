810e1880972d6db2c24c833688aff722f3fac524
Mike Klein <mtklein@google.com>
2021-02-23 09:30:03 -0600

    roll xcode to 12.5
    Just housekeeping while I'm in here.
    
    Change-Id: Id49c43365e5e7e97a87fe05d82fb6f26ce1f2adc

--- infra/bots/bot.py
+++ infra/bots/bot.py
@@ -21,8 +21,8 @@ if 'darwin' in sys.platform:
   mac_toolchain = os.path.join(os.getcwd(), sys.argv[3])
   xcode_app_path = os.path.join(os.getcwd(), sys.argv[4])
   # See mapping of Xcode version to Xcode build version here:
-  # https://chromium.googlesource.com/chromium/tools/build/+/master/scripts/slave/recipe_modules/ios/api.py#37
-  XCODE_BUILD_VERSION = '11c29'
+  # https://chrome-infra-packages.appspot.com/p/infra_internal/ios/xcode/mac/+/
+  XCODE_BUILD_VERSION = '12e5220o'   # xcode 12.5
   call(('{mac_toolchain}/mac_toolchain install '
         '-kind mac '
         '-xcode-version {xcode_build_version} '
