46566e9907bc5d84cff5f1c066b5ab36dc663d03
Leandro Lovisolo <lovisolo@google.com>
2022-04-12 04:08:03 +0000

    [bazel] Add hermetic Android NDK C++ toolchain.
    See //toolchain/README.md to understand how I arrived at this design.
    
    This toolchain can be tested via the following Bazel invocations:
    
        $ bazel build //... --config=android-arm-hermetic
        $ bazel build //... --config=android-arm-hermetic-rbe
        $ bazel build //... --config=android-arm64-hermetic
        $ bazel build //... --config=android-arm64-hermetic-rbe
    
    In a follow-up CL I will delete the non-hermetic toolchain that uses the `android_ndk_repository`[1] built-in Bazel rule, and I will update the skcms-Android-ARM-Bazel-Build-RBE and skcms-Android-ARM64-Bazel-Build-RBE tasks to use the hermetic toolchain.
    
    [1] https://bazel.build/reference/be/android#android_ndk_repository
    
    Bug: skia:12400
    Change-Id: I917c65a64e5e6320842d25940b2ca5477073f577

---
+++ toolchain/trampolines/aarch64-linux-android-ar.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+external/android_ndk/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/aarch64-linux-android-ar $@
---
+++ toolchain/trampolines/arm-linux-androideabi-objcopy.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+external/android_ndk/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin/arm-linux-androideabi-objcopy $@
