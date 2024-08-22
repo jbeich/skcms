f96615e73170156231dae062d4f2b30beb6ddb7f
Brian Osman <brianosman@google.com>
2024-08-22 16:23:49 -0400

    Add skcms_GetInputChannelCount utility function
    This gives a best guess at the number of expected input channels on
    the A side of the profile, with -1 for failure. We can use this to
    judge if an image's encoding (eg, grayscale or RGB) makes sense
    with the accompanying profile.
    
    Bug: 361116767
    Change-Id: Ic571b43d53179d5ed278b9d1d23fff17de046361
    Reviewed-on: https://skia-review.googlesource.com/c/skcms/+/892757
    Commit-Queue: Brian Osman <brianosman@google.com>
    Reviewed-by: Ben Wagner <bungeman@google.com>

--- test_only.c
+++ test_only.c
@@ -196,6 +196,7 @@ void dump_profile(const skcms_ICCProfile* profile, FILE* fp) {
     fprintf(fp, "%20s : 0x%08X : %u\n", "Size", profile->size, profile->size);
     dump_sig_field(fp, "Data color space", profile->data_color_space);
     dump_sig_field(fp, "PCS", profile->pcs);
+    fprintf(fp, "%20s :            : %d\n", "Input Channel Count", skcms_GetInputChannelCount(profile));
     fprintf(fp, "%20s : 0x%08X : %u\n", "Tag count", profile->tag_count, profile->tag_count);
 
     fprintf(fp, "\n");
