743d9ef12b0251537ed8c660a1fe852296dafff4
Eric Boren <borenet@google.com>
2025-12-30 18:28:14 +0000

    [bazel] Migrate to Bazel 8.5.0
    Bug: skia:458681039
    Change-Id: I28cdcc92e24c4fc926f6354d44ea40a27a96c080

--- toolchain/android_trampolines/aarch64-linux-android-ld.sh
+++ toolchain/android_trampolines/aarch64-linux-android-ld.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-external/ndk_linux_amd64/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/aarch64-linux-android-ld $@
+external/+download_toolchains_for_skcms+ndk_linux_amd64/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/aarch64-linux-android-ld $@
--- toolchain/android_trampolines/arm-linux-androideabi-objdump.sh
+++ toolchain/android_trampolines/arm-linux-androideabi-objdump.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-external/ndk_linux_amd64/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin/arm-linux-androideabi-objdump $@
+external/+download_toolchains_for_skcms+ndk_linux_amd64/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin/arm-linux-androideabi-objdump $@
